7a8945f39f1313e466196534e61e566d2bc76ba6
DolgorukovAndrey <a88318302@gmail.com>
2025-10-09 10:40:14 +0500

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,19 +1,19 @@
-public class Person extends Name {
-
+public class Person {
+    private Name fullName;
     private int height;
 
     public Person() {
-        super(null,null,null);
+        this.fullName = new Name();
         this.height = 0;
     }
 
     public Person(String name, int height){
-        super(null,name,null);
+        this.fullName = new Name(null,name,null);
         setHeight(height);
     }
 
     public Person(String surname, String name, String patronymic, int height){
-        super(surname,name,patronymic);
+        this.fullName = new Name(surname,name,patronymic);
         setHeight(height);
     }
 
@@ -26,11 +26,33 @@ public class Person extends Name {
         }
     }
 
+    public void setName(String name){
+        fullName.setName(name);
+    }
+
+    public void setSurname(String surname){
+        fullName.setSurname(surname);
+    }
+
+    public void setPatronymic(String patronymic){
+        fullName.setPatronymic(patronymic);
+    }
+
+    public void setFullName(String surname, String name, String patronymic){
+        fullName.setName(name);
+        fullName.setSurname(surname);
+        fullName.setPatronymic(patronymic);
+    }
+
+    public Name getFullName(){
+        return fullName;
+    }
+
     public int getHeight() {
         return height;
     }
 
     public String toString() {
-        return super.toString() + ", рост: " + height;
+        return fullName.toString() + ", рост: " + height;
     }
 }
